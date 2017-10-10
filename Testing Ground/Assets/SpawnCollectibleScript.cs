23050117597be817f7ac279fb8476db888bb9924
NullTerra180 <nullterra180@gmail.com>
2017-10-10 01:06:23 -0400

    Designed prototype urban environment into Testing. Created Thief, Cop, Employee agents. Added RAIN AI Engine. Added script to spawn rotating gold coin in one random room after 1.5 seconds. Added 3 Waypoint Routes for cop agents and 1 Waypoint Network for thief agent. Deleted previous agents and points. TODO: learn how to use RAIN to define behavior trees for agents. Will work on making shopper agents spawning script next. -Sam

---
+++ Testing Ground/Assets/SpawnCollectibleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCollectibleScript : MonoBehaviour {
+
+	//create an array of spawn points, assigned in inspector
+	public Transform[] SpawnPointArray;
+	//wait time before coin is spawned
+	public float spawnTime = 1.5f;
+
+	public GameObject Coin;
+
+	void Start()
+	{
+		Invoke ("SpawnCoin", spawnTime);
+	}
+	
+	void Update () {
+		
+	}
+
+	void SpawnCoin() {
+		int spawnIndex = Random.Range(0,SpawnPointArray.Length);
+		Instantiate (Coin,SpawnPointArray[spawnIndex].position,SpawnPointArray[spawnIndex].rotation);
+	}
+}
